aafca12c707c2b4dae613f97a01ef08f50c23a9c
Vivek <vivek@deforay.com>
2018-09-25 19:29:16 +0530

    Small changes is Decrypt clinic grid

--- module/Application/src/Application/View/Helper/HumanDateFormat.php
+++ module/Application/src/Application/View/Helper/HumanDateFormat.php
@@ -6,14 +6,14 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\View\Helper\AbstractHelper;
 
 class HumanDateFormat extends AbstractHelper implements ServiceLocatorAwareInterface{
-    
+
     public function setServiceLocator(ServiceLocatorInterface $serviceLocator){
-        $this->serviceLocator = $serviceLocator;  
-        return $this;  
+        $this->serviceLocator = $serviceLocator;
+        return $this;
     }
-    
+
     public function getServiceLocator(){
-        return $this->serviceLocator;  
+        return $this->serviceLocator;
     }
     // $inputFormat = yyyy-mm-dd
     public function __invoke($date){
