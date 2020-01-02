f1fe2a074e1d97d9d8276a1e8e885f5ba213f4db
Thanaseelan <thanaseelan@deforay.com>
2020-01-02 16:04:27 +0530

    Few chnages done

--- config/autoload/custom.global.php
+++ config/autoload/custom.global.php
@@ -3,6 +3,7 @@ return array(
     'defaults' => array(
         'dbsId' => 1,
         'plasmaId' => 2,
-        'tat-skipdays' => 0
+        'tat-skipdays' => 0,
+        'use-dash-current-table' => false
     ),
 );
