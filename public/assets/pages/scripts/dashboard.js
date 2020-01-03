f2888eb83c2756173ae31b29136223cc74a4b31d
Thanaseelan <thanaseelan@deforay.com>
2020-01-03 15:19:30 +0530

    Date range check while slecting the current status

--- public/assets/pages/scripts/dashboard.js
+++ public/assets/pages/scripts/dashboard.js
@@ -1,31 +1,19 @@
 var Dashboard = function() {
-
     return {
-
         initDashboardDaterange: function() {
             if (!jQuery().daterangepicker) {
                 return;
             }
-
-            
-
             $('#dashboard-report-range span').html(moment().subtract('days', 29).format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
             $('#dashboard-report-range').show();
         },
 
-
         init: function() {
-
             this.initDashboardDaterange();
         }
     };
-
 }();
-
 
 jQuery(document).ready(function() {
    Dashboard.init();
 });
-
-
-
