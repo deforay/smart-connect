6c8815b49000995ba9682e90b500b35ec60c7356
Amit Dugar <amit@deforay.com>
2020-06-16 21:19:51 +0530

    Added shell script to create single year table

---
+++ scripts/daily.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MYSQL_PATH="mysql"
+MYSQL_HOST="localhost"
+MYSQL_DATABASE="vldashboard"
+MYSQL_USER="root"
+MYSQL_PASSWORD="mko)(*&^"
+MYSQL_PORT=3306
+
+$MYSQL_PATH --user=$MYSQL_USER --password=$MYSQL_PASSWORD --database=$MYSQL_DATABASE --execute="DROP TABLE if exists dash_vl_request_form_current; CREATE TABLE dash_vl_request_form_current LIKE dash_vl_request_form; INSERT dash_vl_request_form_current SELECT * FROM dash_vl_request_form where sample_collection_date >= DATE_SUB(now(), INTERVAL 12 MONTH);"
