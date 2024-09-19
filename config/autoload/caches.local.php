d910a601b6d880d0b2b0d730df616efc61823f51
Amit Dugar <amit@deforay.com>
2024-09-19 15:00:38 +0530

    Blackhole Cache added for Development mode

--- config/autoload/caches.local.php
+++ config/autoload/caches.local.php
@@ -1,35 +1,40 @@
 <?php
-$isDevelopment = getenv('APPLICATION_ENV') === 'development';
+
+$isDevelopment = file_exists(__DIR__ . '/../development.config.php') || getenv('APPLICATION_ENV') === 'development';
 
 return [
     'caches' => [
         'Cache\Persistent' => [
             'adapter' => $isDevelopment ? 'Laminas\Cache\Storage\Adapter\BlackHole' : 'Laminas\Cache\Storage\Adapter\Filesystem',
-            'options' => [
-                'cache_dir' => getcwd() . "/data/cache/",
-                'dir_permission' => 0755,
-                'file_permission' => 0666,
-                'dir_level' => 1,
-                'clear_stat_cache' => true,
-                'ttl' => 1440, // 1440 minutes = 1 day
-            ],
-            'plugins' => [
-                [
-                    'name' => 'exception_handler',
-                    'options' => [
-                        'throw_exceptions' => true,
+            'options' => $isDevelopment
+                ? [] // No options required for BlackHole
+                : [
+                    'cache_dir' => getcwd() . "/data/cache/",
+                    'dir_permission' => 0755,
+                    'file_permission' => 0666,
+                    'dir_level' => 1,
+                    'clear_stat_cache' => true,
+                    'ttl' => 1440, // 1440 minutes = 1 day
+                ],
+            'plugins' => $isDevelopment
+                ? [] // No plugins for BlackHole adapter
+                : [
+                    [
+                        'name' => 'exception_handler',
+                        'options' => [
+                            'throw_exceptions' => true,
+                        ],
+                    ],
+                    [
+                        'name' => 'serializer'
+                    ],
+                    [
+                        'name' => 'clearExpiredByFactor',
+                        'options' => [
+                            'clearing_factor' => 10,
+                        ],
                     ],
                 ],
-                [
-                    'name' => 'serializer'
-                ],
-                [
-                    'name' => 'clearExpiredByFactor',
-                    'options' => [
-                        'clearing_factor' => 1,
-                    ],
-                ],
-            ]
-        ]
+        ],
     ],
 ];
